82a3038f5fd66c251c317110b7327b8c0d55f5ad
Elazar Nudell <elazar.nudell@gmail.com>
2013-05-19 12:53:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yuni::Application.config.secret_token = '5521f2e3c7fabcf9f2c721219e7f433591d878d8eac5ed49cac9760a2057c177a05359bbaa4170393d0e1b2cb3dc2ff3c38b94652369a679b8f29bf0e22059ce'
